7f99d557b685c29162c198f3746891833998a9de
Michael Taylor <mike_taylor308@hotmail.co.uk>
2020-09-19 14:18:51 +1200

    Add project files.

---
+++ ToDo/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [StringLength(200, ErrorMessage ="You did a bad thing")]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Due { get; set; }
+    }
+}
